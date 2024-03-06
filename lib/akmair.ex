18fd9580acf68624add2d7694aab625a3dea0bf1
vidurkhanal <vkhanal@caldwell.edu>
2024-03-05 23:19:49 -0500

    chore: init a mix project

---
+++ lib/akmair.ex
@@ -0,0 +1,18 @@
+defmodule Akmair do
+  @moduledoc """
+  Documentation for `Akmair`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Akmair.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/akmair_test.exs
@@ -0,0 +1,8 @@
+defmodule AkmairTest do
+  use ExUnit.Case
+  doctest Akmair
+
+  test "greets the world" do
+    assert Akmair.hello() == :world
+  end
+end
